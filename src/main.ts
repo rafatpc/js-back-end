f7e7e4dd44d06e7262c3a4c5bf2d327b2b8b2570
Liliyan Krumov <liliyan.krumov@puzzel.com>
2020-04-22 21:07:17 +0300

    Move the app.module inside of the modules directory

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { Logger, ClassSerializerInterceptor } from '@nestjs/common';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
 
 const PORT = 3000;
 
