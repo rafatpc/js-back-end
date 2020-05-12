ad33e4aeffecf9e8b3b8afb3655d72d4f0d19db0
Liliyan Krumov <liliyan.krumov@puzzel.com>
2020-05-12 23:43:10 +0300

    Restructure

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -1,16 +1,30 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { RESET_CONDITIONS_MOCK, RESET_REWARDS_MOCK } from 'src/condition-engine/mocks';
 
+import { Controller, Get, Post, Param, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { InjectConnection } from '@nestjs/typeorm';
+import { Repository, Connection } from 'typeorm';
+
+import { ConditionEngine } from 'src/condition-engine/condition.engine';
+import { MEMB_STAT } from 'src/database/memb-stat.entity';
 import { MEMB_INFO } from 'src/database/memb-info.entity';
+
+import { CharacterService } from '../character/character.service';
 import { User } from '../user/user.decorator';
 import { AccountService } from './account.service';
 
 @Controller('account')
 @UseGuards(AuthGuard('jwt'))
 export class AccountController {
+    private status: Repository<MEMB_STAT>;
+
     constructor(
+        @InjectConnection() private connection: Connection,
+        private readonly characterService: CharacterService,
         private accountService: AccountService
-    ) { }
+    ) {
+        this.status = this.connection.getRepository(MEMB_STAT);
+    }
 
     @Get('characters')
     characters(@User() user: MEMB_INFO) {
@@ -22,5 +36,38 @@ export class AccountController {
         console.log(user.memb___id);
 
         return this.accountService.getCharacter(user.memb___id, name);
+    }
+
+    // TODO: Verify that the character belongs to the account!
+    @Post('character/:name/:module')
+    @UseGuards(AuthGuard('jwt'))
+    async module(@Param('name') name) {
+        const Character = await this.characterService.findOne(name);
+        const Status = await this.status.findOne(Character.AccountID);
+
+        const conditions = new ConditionEngine(RESET_CONDITIONS_MOCK, {
+            Character, Status
+        });
+
+        return conditions.satisfy();
+    }
+
+    // TODO: Verify that the character belongs to the account!
+    @Get('character/:name/:module')
+    @UseGuards(AuthGuard('jwt'))
+    async moduleConfig(@Param('name') name) {
+        const Character = await this.characterService.findOne(name);
+        const Status = await this.status.findOne(Character.AccountID);
+
+        const conditions = new ConditionEngine(RESET_CONDITIONS_MOCK, {
+            Character, Status
+        });
+
+        const result = conditions.check();
+
+        return {
+            conditions: result,
+            rewards: RESET_REWARDS_MOCK
+        };
     }
 }
--- src/modules/character/character.controller.ts
+++ src/modules/character/character.controller.ts
@@ -1,26 +1,14 @@
-import { RESET_CONDITIONS_MOCK, RESET_REWARDS_MOCK } from 'src/condition-engine/mocks';
-
 import { Controller, Get, Post, Param, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { InjectConnection } from '@nestjs/typeorm';
-import { Repository, Connection } from 'typeorm';
 
-import { MEMB_STAT } from 'src/database/memb-stat.entity';
 import { Character } from 'src/database/character.entity';
-import { ConditionEngine } from 'src/condition-engine/condition.engine';
-
 import { CharacterService } from './character.service';
 
 @Controller('character')
 export class CharacterController {
-    private status: Repository<MEMB_STAT>;
-
     constructor(
         private readonly characterService: CharacterService,
-        @InjectConnection() private connection: Connection
-    ) {
-        this.status = this.connection.getRepository(MEMB_STAT);
-    }
+    ) { }
 
     @Get(':name')
     get(@Param('name') name): Promise<Character> {
@@ -39,38 +27,5 @@ export class CharacterController {
     @UseGuards(AuthGuard('jwt'))
     extended(@Param('name') name): Promise<Character> {
         return this.characterService.findOne(name);
-    }
-
-    // TODO: Verify that the character belongs to the account!
-    @Post(':name/:module')
-    @UseGuards(AuthGuard('jwt'))
-    async module(@Param('name') name) {
-        const Character = await this.characterService.findOne(name);
-        const Status = await this.status.findOne(Character.AccountID);
-
-        const conditions = new ConditionEngine(RESET_CONDITIONS_MOCK, {
-            Character, Status
-        });
-
-        return conditions.satisfy();
-    }
-
-    // TODO: Verify that the character belongs to the account!
-    @Get(':name/:module')
-    @UseGuards(AuthGuard('jwt'))
-    async moduleConfig(@Param('name') name) {
-        const Character = await this.characterService.findOne(name);
-        const Status = await this.status.findOne(Character.AccountID);
-
-        const conditions = new ConditionEngine(RESET_CONDITIONS_MOCK, {
-            Character, Status
-        });
-
-        const result = conditions.check();
-
-        return {
-            conditions: result,
-            rewards: RESET_REWARDS_MOCK
-        };
     }
 }
